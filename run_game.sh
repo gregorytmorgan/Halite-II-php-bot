fe026938e1d3251bd35ebf6090e91374d5056d7f
Greg Morgan <gregorytmorgan@gmail.com>
2017-12-29 17:17:16 -0700

    Add seed param

--- run_game.sh
+++ run_game.sh
@@ -2,5 +2,11 @@
 
 export HALITE_PHP_ENV=dev
 
+if [ "$1" == "" ]; then
+  SEED=""
+else
+  SEED="-s 1621542274"
+fi
+
 #./halite -s 1621542274 -d "240 160" "php MyBot.php" "php MyBot.php"
-./halite -d "240 160" "php MyBot.php" "php MyBot.php"
+./halite $SEED -d "240 160" "php MyBot.php" "php MyBot.php"
