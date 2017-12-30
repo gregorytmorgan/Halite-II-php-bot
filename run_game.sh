a63ebffbe9d511751b1ebc8393fbef07a05919b5
Greg Morgan <gregorytmorgan@gmail.com>
2017-12-29 21:51:37 -0500

    Tweak run_game

--- run_game.sh
+++ run_game.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-
+#
+# Run a Halite game
+#
+# Pass in SEED to reproduce the same game. Pass in 0 to produce
+# a random new game, nothing to produce the default game/seed.  
+#
 export HALITE_PHP_ENV=dev
 
 if [ "$1" == "" ]; then
-  SEED=""
-else
-  SEED="-s 1621542274"
+    SEED="-s 1621542274"
+elif [ "$1" == "0" ]; then
+    SEED=""    
+else 
+    SEED=" -s $1"
 fi
 
 #./halite -s 1621542274 -d "240 160" "php MyBot.php" "php MyBot.php"
